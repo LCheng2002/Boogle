17d2acdd532aa3035d204e2d1e1df34a7061d87a
chengstones <chenglei20020408@sjtu.edu.cn>
2022-01-01 23:55:30 +0800

    main

--- time.bat
+++ time.bat
@@ -1,13 +1,12 @@
 @echo off
 title 
-C:\Program Files\Git\bin\git.exe pull origin master
+git pull origin master
 
 C:\ProgramData\Anaconda3\python.exe phrase_comment.py
 
-set /p commit=commit:
 title auto commit
-C:\Program Files\Git\bin\git.exe add -A
-C:\Program Files\Git\bin\git.exe commit -m %commit%
-C:\Program Files\Git\bin\git.exe push origin main
+git add -A
+git commit -m main
+git push origin main
 exit
 
