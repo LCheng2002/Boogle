45b00e4ab13496b39b7a39b9806406c96015045f
chengstones <chenglei20020408@sjtu.edu.cn>
2022-01-02 09:50:11 +0800

    main

--- check_DB.py
+++ check_DB.py
@@ -6,10 +6,10 @@ def check_DB(product_id):
     connect = sqlite3.connect('./jingdong_comment.db')
     cursor = connect.cursor()
 
-    # sql = "select name,comment from jingdong_comment where jingdong_comment.product_id = {}".format(product_id)
-    sql = "select product_id,name,comment from jingdong_comment where jingdong_comment.product_id = {}".format(product_id)
+    sql = "select product_id from jingdong_comment"
+    # sql = "select product_id,name,comment from jingdong_comment where jingdong_comment.product_id = {}".format(product_id)
     cursor.execute(sql)
-    print(cursor.fetchall())
+    print(len(set(cursor.fetchall())))
 
     cursor.close()
     connect.close()
