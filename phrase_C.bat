29074e7b99bca6b923d114cd8af6251a5364297b
chengstones <chenglei20020408@sjtu.edu.cn>
2022-01-02 11:50:51 +0800

    main

--- phrase_C.bat
+++ phrase_C.bat
@@ -1,12 +1,10 @@
 @echo off
 cd C:\Users\cheng\Desktop\Boogle
 
-title auto pull
 git pull origin main
 
 start C:\ProgramData\Anaconda3\python.exe .\phrase_comment.py
 
-title auto commit
 git add .
 git commit -m main
 git push origin main
