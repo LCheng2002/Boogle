0a9085039426a989e2e75846a7a72b8a3a085af7
chengstones <chenglei20020408@sjtu.edu.cn>
2022-01-02 14:50:21 +0800

    main

--- push_to_history.py
+++ push_to_history.py
@@ -1,5 +1,4 @@
 import sqlite3
-from sqlite3.dbapi2 import Time
 import time
 
 def push_to_history(content):
@@ -8,16 +7,18 @@ def push_to_history(content):
     Time = time.strftime('%Y-%m-%d %H:%M:%S')
 
     cursor.execute("""
-        CREATE TABLE IF NOT EXISTS jingdong_book(
+        CREATE TABLE IF NOT EXISTS history(
             id INTEGER PRIMARY KEY,
             content TEXT,
             Time TEXT,
         );
     """)
 
-    cursor.execute("""INSERT INTO jingdong_book (searching_content,Time) VALUES (?,?);""",[content,Time])
+    cursor.execute("""INSERT INTO history (searching_content,Time) VALUES (?,?);""",[content,Time])
 
     connect.commit()
 
     cursor.close()
     connect.close()
+
+push_to_history("足球")
